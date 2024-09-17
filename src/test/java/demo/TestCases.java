90093534cbac5c994a2fab8067b984da1f297238
Nitish <nitishlikes@gmail.com>
2024-09-18 02:00:33 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -9,30 +9,63 @@ import org.openqa.selenium.logging.LoggingPreferences;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.testng.Assert;
+import java.time.Duration;
 import java.util.logging.Level;
-
-
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
+
 
 public class TestCases {
     ChromeDriver driver;
 
-    /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
-     */
+    @Test
+    public void testCase01() throws InterruptedException {
 
+        System.out.println("Start TestCase 01");
+
+        boolean status;
+        driver.get("https://www.flipkart.com");
+        Wrappers.enterText(driver, By.cssSelector(".Pke_EE"), "Washing Machine");
+        Thread.sleep(3000);
+        Wrappers.clickElement(driver, By.xpath("//div[text()='Popularity']"));
+        status = Wrappers.productRating(driver, By.xpath("//span[contains(@id,'productRating')]/div"), 4.0);
+       
+
+        System.out.println("End TestCase 01");
+    }
+
+    @Test
+    public void testCase02() throws InterruptedException {
+        System.out.println("Start TestCase 02");
+
+        boolean status;
+        driver.get("https://www.flipkart.com");
+        Wrappers.enterText(driver, By.cssSelector(".Pke_EE"), "iPhone");
+        Thread.sleep(3000);
+        status = Wrappers.productTitleAndDiscount(driver, By.xpath("//div[@class='yKfJKb row']"), 17);
+       
+
+        System.out.println("End TestCase 02");
+    }
+    @Test
+    public void testCase03() throws InterruptedException {
+        System.out.println("Start TestCase 03");
+
+        boolean status;
+        driver.get("https://www.flipkart.com");
+        Wrappers.enterText(driver, By.cssSelector(".Pke_EE"), "Coffee Mug");
+        Thread.sleep(3000);
+        Wrappers.clickElement(driver, By.xpath("//div[@class='XqNaEv']"));
+        Thread.sleep(3000);
+        status = Wrappers.productTitleAndImage(driver, By.xpath("//span[@class='Wphh3N']"));
      
-    /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
-     */
+
+        System.out.println("End TestCase 03");
+    }
+
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -49,13 +82,12 @@ public class TestCases {
         System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
 
         driver = new ChromeDriver(options);
-
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
 
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,12 +1,6 @@
 package demo.wrappers;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
+import java.util.*;
+import java.text.NumberFormat;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -16,7 +10,110 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+
+    public static void enterText(WebDriver driver, By locator, String text) {
+        boolean status;
+        try {
+            WebDriverWait  wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            WebElement search = driver.findElement(locator);
+            search.clear();
+            search.sendKeys(text);
+            search.sendKeys(Keys.ENTER);
+            status = true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            status = false;
+        }
+    }
+
+    public static void clickElement(WebDriver driver, By locator) {
+        boolean status;
+        try {
+            WebDriverWait  wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            WebElement element = driver.findElement(locator);
+            element.click();
+            status = true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            status = false;
+        }
+     }
+
+     public static Boolean productRating(WebDriver driver, By locator, double rating) {
+        int count = 0;
+        boolean status;
+        try {
+            List<WebElement> products = driver.findElements(locator);
+            for (WebElement product : products) {
+                if (Double.parseDouble(product.getText()) <= rating)
+                    count++;
+            } status = true;
+            System.out.println("Count of washing machines with rating less than or equal to " + rating + " : " + count);
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+            status = false;
+        }
+        return status;
+     }
+
+     public static Boolean productTitleAndDiscount(WebDriver driver, By locator, int discount) {
+        boolean status;
+        try {
+            HashMap<String, String> map = new HashMap<>();
+            List<WebElement> products = driver.findElements(locator);
+            for(WebElement product : products) {
+               String discountPercent = product.findElement(By.xpath(".//div[@class='UkUFwK']/span")).getText();
+               int discountValue = Integer.parseInt(discountPercent.replaceAll("[^\\d]", ""));
+               if (discountValue > discount) {
+                   String productTitle = product.findElement(By.xpath(".//div[@class='KzDlHZ']")).getText();
+                   map.put(discountPercent, productTitle);
+               }
+            }
+            for (Map.Entry<String, String> productMap : map.entrySet()) 
+                System.out.println("Discount percentage is " + productMap.getKey() + " and the title is " + productMap.getValue());
+            
+             status = true;       
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+            status = false;
+        }
+            return status;
+     }
+
+     public static Boolean productTitleAndImage(WebDriver driver, By locator) {
+        boolean status;
+        try {
+            Set<Integer> set = new HashSet<>();
+            List<WebElement> reviews = driver.findElements(locator);
+            for (WebElement review : reviews) {
+                int userReview = Integer.parseInt(review.getText().replaceAll("[^\\d]", ""));
+                set.add(userReview);
+            }
+            List<Integer> ReviewList = new ArrayList<>(set);
+            Collections.sort(ReviewList,Collections.reverseOrder());
+            System.out.println(ReviewList);
+            NumberFormat numberFormat = NumberFormat.getInstance(Locale.US);
+            LinkedHashMap<String, String> productDetailsMap = new LinkedHashMap<>();
+            for (int i = 0; i < 5; i++) {
+                String formattedUserReviewCount = "(" + numberFormat.format(ReviewList.get(i)) + ")";
+                String productTitle = driver.findElement(By.xpath("//div[@class='slAVV4']//span[contains(text(),'"
+                        +formattedUserReviewCount+ "')]/../../a[@class='wjcEIp']")).getText();
+                String productImgURL = driver.findElement(By.xpath("//div[@class='slAVV4']//span[contains(text(),'"
+                        +formattedUserReviewCount+ "')]/../..//img[@class='DByuf4']")).getAttribute("src");
+                String hightestReviewCountAndProductTitle = String.valueOf(i+1)+" highest review count: "+formattedUserReviewCount+" Title: "+productTitle;
+                productDetailsMap.put(hightestReviewCountAndProductTitle, productImgURL);
+            }
+            //print title and image url
+            for(Map.Entry<String,String> productDetails : productDetailsMap.entrySet()){
+                System.out.println(productDetails.getKey()+" and Product image url : "+productDetails.getValue());
+            }
+            status = true;
+        } catch(Exception e) {
+            e.printStackTrace();
+            status = false;
+        }
+        return status;
+     }
 }
